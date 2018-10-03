1c76f35b364ae5d578d853cfd482f981d4a80568
Tomasz Lis <tomasz.lis@o2.pl>
2018-10-04 00:08:13 +0200

    Add SCSS files

--- js/script.js
+++ js/script.js
@@ -4,19 +4,24 @@ var movies = [
 	{
 		id: 1,
 		title: 'Harry Potter',
-		desc: 'Film o czarodzieju'
+		desc: 'Film o czarodzieju',
+		image: 'images/harry_potter.jpg'
 	},
 	{
 		id: 2,
 		title: 'Król Lew',
-		desc: 'Film opowiadający historię króla sawanny'
+		desc: 'Film opowiadający historię króla sawanny',
+		image: 'images/lion_king.jpg'
 	}
 ];
 
 var moviesElements = movies.map(function (movie) {
 	return React.createElement('li', {key: movie.id},
 		React.createElement('h2', {}, movie.title),
-		React.createElement('p', {}, movie.desc)
+		React.createElement('p', {}, movie.desc),
+		React.createElement('img', {
+			src: movie.image,
+			})
 		);
 });
 
